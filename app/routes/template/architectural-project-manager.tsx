cee8515e13deb0537b3b67a58a1eec96e6713e5f
SALEHIN <msionlinekingdom@gmail.com>
2025-05-06 07:49:54 +0600

    architechtural project manager cv initiated

--- app/routes/template/architectural-project-manager.tsx
+++ app/routes/template/architectural-project-manager.tsx
@@ -1,4 +1,9 @@
+import { BiPhone } from 'react-icons/bi';
 import type { Route } from './+types/architectural-project-manager';
+import { MdAlternateEmail } from 'react-icons/md';
+import { Link } from 'react-router';
+import { FaLink } from 'react-icons/fa';
+import { FaLocationDot } from 'react-icons/fa6';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,6 +14,52 @@ export function meta({}: Route.MetaArgs) {
 
 export default function ArchitecturalProjectManager() {
   return (
-    <div>architectural-project-manager</div>
+    <div id='cv-content' className='w-[794px] bg-[#C3C3C3]'>
+        <div className="grid grid-cols-3">
+            <div className="col-span-2 pl-[60px] pt-[72px] pb-[72px] pr-[10px]">
+{/* name */}
+<h1 className="uppercase text-3xl text-[#2E3639] font-bold">Carter Andrade</h1>
+            {/* title/ designation */}
+      <p className="text-[#1A92C3] mt-2.5 text-xl font-bold">
+      Architectural Project Manager
+      </p>
+      {/* number, email, link */}
+      <div className="flex flex-col w-full text-sm">
+        <div className="flex flex-wrap justify-between">
+          {/* phone */}
+          <div className="flex flex-row gap-1 items-center ">
+            <BiPhone className="text-[#9B9B9B]" />
+            <p className="text-[#373F6F] font-semibold">+1-541-754-3010</p>
+          </div>
+          {/* email */}
+          <div className="flex flex-row gap-1 items-center">
+            <MdAlternateEmail className="text-[#9B9B9B] " />
+            <Link
+              to={"mailto:help@professionalupdate.com"}
+              className="text-[#373F6F] font-semibold"
+            >
+              help@professionalupdate.com
+            </Link>
+          </div>
+          {/* linkedIn */}
+          <div className="flex flex-row gap-1 items-center">
+            <FaLink className="text-[#9B9B9B]" />
+            <Link to={"linkedin.com"} className="text-[#373F6F] font-semibold">
+              linkedin.com
+            </Link>
+          </div>
+          {/* location */}
+          <div className="flex flex-row gap-1 items-center">
+            <FaLocationDot className="text-[#9B9B9B] " />
+            <p className="text-[#373F6F] font-semibold">San Francisco, CA</p>
+          </div>
+        </div>
+      </div>
+            </div>
+            <div className="bg-[#1D3449] pr-[72px] pt-[72px] pb-[72px]">
+
+            </div>
+        </div>
+    </div>
   )
 }
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router";
 
-const CVList:{img:string,title:string,url:string}[]=[{
-  img:"https://raw.githubusercontent.com/SALEHINISLAM/cv-template/main/public/engineer_10899202.png",
-  title:"Civil Engineer CV",
-  url:"/civil-engineer-cv"
-},{
-  img:"https://raw.githubusercontent.com/SALEHINISLAM/cv-template/main/public/ArchitecturalProjectManager.png",
-  title:"Architectural Project Manager CV",
-  url:"/architectural-project-manager-cv"
-}]
+const CVList: { img: string; title: string; url: string }[] = [
+  {
+    img: "https://raw.githubusercontent.com/SALEHINISLAM/cv-template/main/public/engineer_10899202.png",
+    title: "Civil Engineer CV",
+    url: "/civil-engineer-cv",
+  },
+  {
+    img: "https://raw.githubusercontent.com/SALEHINISLAM/cv-template/main/public/ArchitecturalProjectManager.png",
+    title: "Architectural Project Manager CV",
+    url: "/architectural-project-manager-cv",
+  },
+];
 
 export function Welcome() {
   return (
     <main className="container mx-auto pt-16 pb-4">
-      <ol className="list-decimal w-full max-w-2xl p-4">
-        <li className="flex items-center gap-2 ">
-          <img className="w-16 h-16" src="https://raw.githubusercontent.com/SALEHINISLAM/cv-template/main/public/engineer_10899202.png" alt="" />
-          <b>Civil Engineer CV:</b>
-          <Link to={"/civil-engineer-cv"} className="underline">Click Here</Link>
-        </li>
-
+      <ol className="list-decimal w-full max-w-2xl p-4 min-h-screen">
+        {CVList.map((cv, index) => (
+          <li key={index} className="flex items-center gap-2 ">
+            <img className="w-16 h-16" src={cv.img} alt="" />
+            <b>{cv.title}:</b>
+            <Link to={cv.url} className="underline">
+              Click Here
+            </Link>
+          </li>
+        ))}
       </ol>
+      {/* footer */}
+      <div className="mb-16 text-center">Github Repository:<Link to={'https://github.com/SALEHINISLAM/cv-template'}>https://github.com/SALEHINISLAM/cv-template</Link></div>
     </main>
   );
 }
